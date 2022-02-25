fcc01ed4fa56ca1d24979de8526a1ae566a10c97
Rizvan <rizv0001@easv365.dk>
2022-02-25 11:33:24 +0100

    Order status enums

--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static OrderApi.Enums.Enums;
+
 namespace OrderApi.Models
 {
     public class Order
@@ -7,5 +9,6 @@ namespace OrderApi.Models
         public DateTime? Date { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+        public OrderStatus Status { get; set; }
     }
 }
