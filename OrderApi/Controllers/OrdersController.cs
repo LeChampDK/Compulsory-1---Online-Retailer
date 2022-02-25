687eecb9ef2cc43bb96ee75cf626e59c3820f589
René Jørgensen <rene686f@easv365.dk>
2022-02-25 11:33:22 +0100

    Order products

--- OrderApi/Controllers/OrdersController.cs
+++ OrderApi/Controllers/OrdersController.cs
@@ -51,23 +51,27 @@ namespace OrderApi.Controllers
             // You may need to change the port number in the BaseUrl below
             // before you can run the request.
             c.BaseUrl = new Uri("https://localhost:5001/products/");
-            var request = new RestRequest(order.ProductId.ToString(), Method.GET);
-            var response = c.Execute<Product>(request);
-            var orderedProduct = response.Data;
 
-            if (order.Quantity <= orderedProduct.ItemsInStock - orderedProduct.ItemsReserved)
+            foreach(var prod in order.Products)
             {
-                // reduce the number of items in stock for the ordered product,
-                // and create a new order.
-                orderedProduct.ItemsReserved += order.Quantity;
-                var updateRequest = new RestRequest(orderedProduct.Id.ToString(), Method.PUT);
-                updateRequest.AddJsonBody(orderedProduct);
-                var updateResponse = c.Execute(updateRequest);
+                var request = new RestRequest(prod.ProductId.ToString(), Method.GET);
+                var response = c.Execute<Product>(request);
+                var orderedProduct = response.Data;
 
-                if (updateResponse.IsSuccessful)
+                if (prod.Quantity <= orderedProduct.ItemsInStock - orderedProduct.ItemsReserved)
                 {
-                    var newOrder = repository.Add(order);
-                    return CreatedAtRoute("GetOrder", new { id = newOrder.Id }, newOrder);
+                    // reduce the number of items in stock for the ordered product,
+                    // and create a new order.
+                    orderedProduct.ItemsReserved += prod.Quantity;
+                    var updateRequest = new RestRequest(orderedProduct.Id.ToString(), Method.PUT);
+                    updateRequest.AddJsonBody(orderedProduct);
+                    var updateResponse = c.Execute(updateRequest);
+
+                    if (updateResponse.IsSuccessful)
+                    {
+                        var newOrder = repository.Add(order);
+                        return CreatedAtRoute("GetOrder", new { id = newOrder.Id }, newOrder);
+                    }
                 }
             }
 
--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace OrderApi.Models
 {
     public class Order
     {
         public int Id { get; set; }
         public DateTime? Date { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public List<OrderProducts> Products { get; set; }
     }
 }
